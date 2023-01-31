8ed1c8197f7a909a86f63b47ab66bd87af859aaa
jyothi1015 <98731361+jyothi1015@users.noreply.github.com>
2023-01-31 14:29:58 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export function Student(props){
+
+    const {id,name,marks} = props
+
+    return(
+
+        <div>
+
+        <p>{id}</p>
+
+        <p>{name}</p>
+
+        <p>{marks}</p>
+
+        </div>
+
+    )
+
+}
